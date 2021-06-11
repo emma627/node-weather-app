a446a22d1eefe960defcf9bc3fef94c9ce46e65c
jinyan YANG <jinyan.yang627@gmail.com>
2021-06-11 11:36:11 +0200

    views hbs set up

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,11 +3,33 @@ const app = express()
 const path =require('path')
 
 
-
+//use static files
 const publicDirectoryPath = path.join(__dirname,'../public')
-
 app.use(express.static(publicDirectoryPath))
 
+//dynamic content by handlebars
+app.set('view engine', 'hbs')
+
+app.get('',(req, res)=>{
+    res.render('index', {
+        title:"Weather app",
+        name: "jinyan"
+    })
+})
+
+app.get('/about',(req, res)=>{
+    res.render('about', {
+        title:"About me",
+        name: "jinyan"
+    })
+})
+app.get('/help',(req, res)=>{
+    res.render('help', {
+        title: "Help",
+        msg: "This is a help message",
+        name: "jinyan"
+    })
+})
 
 app.get('/weather', (req, res)=>{
     res.send({
