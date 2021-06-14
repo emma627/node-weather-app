803dc7614e812220ee13ca8fb16db50f0cf8f047
jinyan YANG <jinyan.yang627@gmail.com>
2021-06-14 13:12:27 +0200

    address set

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 const path =require('path')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 
-//use static files
+//static directory
 const publicDirectoryPath = path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath))
+
+//customize view directory
+const viewsPath = path.join(__dirname,'../templates/views')
+app.set('views', viewsPath)
+
+//partial set up
+const partialsPath = path.join(__dirname,"../templates/partials")
+hbs.registerPartials(partialsPath)
 
 //dynamic content by handlebars
 app.set('view engine', 'hbs')
@@ -23,6 +34,7 @@ app.get('/about',(req, res)=>{
         name: "jinyan"
     })
 })
+
 app.get('/help',(req, res)=>{
     res.render('help', {
         title: "Help",
@@ -32,12 +44,54 @@ app.get('/help',(req, res)=>{
 })
 
 app.get('/weather', (req, res)=>{
-    res.send({
-        forecast:'sunny',
-        location: 'nantes'
+    if(!req.query.address){
+        return res.send({
+            error: "you must provide an address"
+        })
+    }
+    geocode(req.query.address,(error, {longitude,latitude,location})=>{
+        if(error){
+            return res.send({error})
+        }
+        forecast(longitude,latitude,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address:req.query.address
 
+            })
+        })
+    })
+    
+})
+
+app.get('/help/*',(req, res)=>{
+    res.render('404', {
+        title:"404 Page",
+        name:"jinyan",
+        msg404:"help article not found"
     })
 })
+
+app.get('/products',(req, res)=>{
+    if(!req.query.search){
+         return res.send({
+             error: "you must provide a search term"
+         })
+    }
+    res.send({products:[]})
+})
+
+app.get('*',(req, res)=>{
+    res.render('404', {
+        title:"404 Page",
+        msg404:"page not found"
+    })
+})
+
 
 app.listen(3000,()=>{
     console.log("the port is on port 3000")
